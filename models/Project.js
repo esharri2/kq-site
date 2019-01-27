391f2574f5b08082c1b1e94bea5c664f97deeee6
esharri2 <esharri2@gmail.com>
2019-01-27 17:01:18 -0500

    project page headings

--- models/Project.js
+++ models/Project.js
@@ -1,42 +1,41 @@
-var keystone = require('keystone');
+var keystone = require("keystone");
 var Types = keystone.Field.Types;
 
-var Project = new keystone.List('Project', {
-    map: {
-        name: 'title'
-    },
+var Project = new keystone.List("Project", {
+  map: {
+    name: "title"
+  }
 });
 
 Project.add({
-    title: {
-        type: String,
-        required: true,
-        initial: true
-    },
-    type: {
-        type: Types.Select,
-        options: 'short film, feature film, play, episodic, web series, other',
-        index: true,
-        initial: true
-    },
-    url: {
-        type: Types.Url,
-        note: 'You must include https:// at the start of the url.'
-    },
-    description: {
-        type: Types.Html,
-        wysiwyg: true
-    },
-    image: {
-        type: Types.CloudinaryImage
-    },
-    releaseDate: {
-        type: Types.Date,
-        default: Date.now
-    }
-
+  title: {
+    type: String,
+    required: true,
+    initial: true
+  },
+  type: {
+    type: Types.Select,
+    options: "films, plays, episodic, web series, other",
+    index: true,
+    initial: true
+  },
+  url: {
+    type: Types.Url,
+    note: "You must include https:// at the start of the url."
+  },
+  description: {
+    type: Types.Html,
+    wysiwyg: true
+  },
+  image: {
+    type: Types.CloudinaryImage
+  },
+  releaseDate: {
+    type: Types.Date,
+    default: Date.now
+  }
 });
 
 //I think this defines what shows in the adminUI
-Project.defaultColumns = 'title, type';
+Project.defaultColumns = "title, type";
 Project.register();
--- routes/views/projects.js
+++ routes/views/projects.js
@@ -1,43 +1,56 @@
-var keystone = require('keystone');
-var moment = require('moment');
-var async = require('async');
+var keystone = require("keystone");
+var moment = require("moment");
+var async = require("async");
 
 module.exports = function(req, res) {
+  var view = new keystone.View(req, res);
+  var locals = res.locals;
+  locals.section = "projects";
 
-    var view = new keystone.View(req, res);
-    var locals = res.locals;
-    locals.section = 'projects';
+  var Project = keystone.list("Project");
 
-    var Project = keystone.list('Project');
+  view.on("init", function(next) {
+    var q = Project.model.find();
+    q.exec(function(err, results) {
+      let data = results.map(result => {
+        const newObject = { ...result._doc };
+        if (result.releaseDate) {
+          const formattedDate = moment(result.releaseDate).format("MMMM YYYY");
+          newObject.formattedDate = formattedDate;
+        } else {
+          newObject.formattedDate = null;
+        }
+        return newObject;
+      });
 
-    view.on('init', function(next) {
-        var q = Project.model.find();
-        q.exec(function(err, results) {
-            let data = results.map((result) => {
-                const newObject = { ...result._doc
-                };
-                const formattedDate = moment(result.releaseDate).format('MMMM YYYY');
-                newObject.formattedDate = formattedDate;
-                return newObject;
-            })
+      let sortedData = data.sort((a, b) => {
+        if (moment(a.releaseDate) > moment(b.releaseDate)) {
+          return -1;
+        }
+        if (a.releaseDate < b.releaseDate) {
+          return 1;
+        }
+        return 0;
+      });
 
-            let sortedData = data.sort((a, b) => {
-                if (moment(a.releaseDate) > moment(b.releaseDate)) {
-                    console.log("gt!");
-                    return -1
-                }
-                if (a.releaseDate < b.releaseDate) {
-                    console.log("lt!");
-                    return 1
-                }
-                console.log("eq!")
-                return 0;
-            })
+      let projects = {
+        films: [],
+        plays: [],
+        episodic: [],
+        "web series": []
+      };
 
-            locals.projects = sortedData;
-            next(err);
-        })
-    })
+      sortedData.forEach(item => {
+        const cat = item.type;
+        projects[cat].push(item);
+      });
 
-    view.render('projects');
+      console.log(projects);
+
+      locals.projects = projects;
+      next(err);
+    });
+  });
+
+  view.render("projects");
 };
