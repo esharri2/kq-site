bf2c7bb67c92eace6408a462170b48cb68375bcf
esharri2 <esharri2@gmail.com>
2019-01-29 21:18:16 -0500

    two section project page

--- routes/views/projects.js
+++ routes/views/projects.js
@@ -1,43 +1,45 @@
-var keystone = require('keystone');
-var moment = require('moment');
-var async = require('async');
+var keystone = require("keystone");
+var moment = require("moment");
+var async = require("async");
 
 module.exports = function(req, res) {
+  var view = new keystone.View(req, res);
+  var locals = res.locals;
+  locals.section = "projects";
 
-    var view = new keystone.View(req, res);
-    var locals = res.locals;
-    locals.section = 'projects';
+  var Project = keystone.list("Project");
 
-    var Project = keystone.list('Project');
+  view.on("init", function(next) {
+    var q = Project.model.find();
+    q.exec(function(err, results) {
+      let data = results.map(result => {
+        const newObject = { ...result._doc };
+        if (result.releaseDate) {
+          const formattedDate = moment(result.releaseDate).format("MMMM YYYY");
+          newObject.formattedDate = formattedDate;
+        } else {
+          newObject.formattedDate = null;
+        }
+        return newObject;
+      });
 
-    view.on('init', function(next) {
-        var q = Project.model.find();
-        q.exec(function(err, results) {
-            let data = results.map((result) => {
-                const newObject = { ...result._doc
-                };
-                const formattedDate = moment(result.releaseDate).format('MMMM YYYY');
-                newObject.formattedDate = formattedDate;
-                return newObject;
-            })
+      let sortedData = data.sort((a, b) => {
+        if (moment(a.releaseDate) > moment(b.releaseDate)) {
+          console.log("gt!");
+          return -1;
+        }
+        if (a.releaseDate < b.releaseDate) {
+          console.log("lt!");
+          return 1;
+        }
+        console.log("eq!");
+        return 0;
+      });
 
-            let sortedData = data.sort((a, b) => {
-                if (moment(a.releaseDate) > moment(b.releaseDate)) {
-                    console.log("gt!");
-                    return -1
-                }
-                if (a.releaseDate < b.releaseDate) {
-                    console.log("lt!");
-                    return 1
-                }
-                console.log("eq!")
-                return 0;
-            })
+      locals.projects = sortedData;
+      next(err);
+    });
+  });
 
-            locals.projects = sortedData;
-            next(err);
-        })
-    })
-
-    view.render('projects');
+  view.render("projects");
 };
